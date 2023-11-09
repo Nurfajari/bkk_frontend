9415ae50cd1d09453ecf1bd7ac4c7e44b0dfc4ac
Nurfajari <azisnurfajari@gmail.com>
2023-11-09 08:52:10 +0700

    p

--- src/Pages/Alumni/Perusahaan.jsx
+++ src/Pages/Alumni/Perusahaan.jsx
@@ -7,6 +7,9 @@ export default function PagePerusahaan()
         <div className="">
             <Navbar></Navbar>
             <div className="">
+                <div className="ms-3 ps-2 border-s-2 border-black">
+                    <h1 className="text-2xl">Daftar Perusahaan</h1>
+                </div>
                 <CardPerusahaan></CardPerusahaan>
                 <CardPerusahaan></CardPerusahaan>
             </div>
