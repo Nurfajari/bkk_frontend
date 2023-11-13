c7d785dfb6a05a6edc67f8dbf930f7abe0621def
Nurfajari <azisnurfajari@gmail.com>
2023-11-14 01:46:29 +0700

    p

--- src/component/Alumni/Navbar.jsx
+++ src/component/Alumni/Navbar.jsx
@@ -9,10 +9,10 @@ export default function Navbar()
                     <img src="/Img/logo.png" className="w-8 rounded-full" alt="" />
                 </div>
                 <div className="flex gap-12 font-bold">
-                    <a href="/alumni" className="hover:bg-black hover:text-white hover:px-5 transition-all ease-in-out delay-150">Home</a>
-                    <a href="/alumni/lowongan" className="hover:bg-black hover:text-white hover:px-5 transition-all ease-in-out delay-150">Lowongan</a>
-                    <a href="/alumni/perusahaan" className="hover:bg-black hover:text-white hover:px-5 transition-all ease-in-out delay-150">Perusahaan</a>
-                    <a href="" className="hover:bg-black hover:text-white hover:px-5 transition-all ease-in-out delay-150">Logout</a>
+                    <a href="/alumni" className="transition-all ease-in-out hover:bg-black hover:text-white hover:px-5 delay-150 duration-75">Home</a>
+                    <a href="/alumni/lowongan" className="transition-all ease-in-out hover:bg-black hover:text-white hover:px-5 delay-150 duration-75">Lowongan</a>
+                    <a href="/alumni/perusahaan" className="transition-all ease-in-out hover:bg-black hover:text-white hover:px-5 delay-150 duration-75">Perusahaan</a>
+                    <a href="" className="transition-all ease-in-out hover:bg-black hover:text-white hover:px-5 delay-150 duration-75">Logout</a>
                 </div>
             </div>
         </nav>
