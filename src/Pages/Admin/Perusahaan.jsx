8ffd1e1fcc78ece1426202524a7ba95aca2880b1
NurThedii <ntterus234@gmail.com>
2023-11-20 13:15:56 +0700

    p

--- src/Pages/Admin/Perusahaan.jsx
+++ src/Pages/Admin/Perusahaan.jsx
@@ -1,8 +1,31 @@
 import { IconSearch, IconSquareRoundedPlus } from '@tabler/icons-react';
 import CardPerusahaan from '../../component/Admin/CardPerusahaan';
 import HeadPage from '../../component/Admin/HeadPage';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Perusahaan = () => {
+  
+  const [perusahaanData, setPerusahaanData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []); // useEffect hanya dijalankan saat komponen dipasang
+
+  useEffect(() => {
+    // useEffect kedua untuk merespon perubahan pada perusahaanData
+    fetchData();
+  }, [perusahaanData]); // useEffect akan dijalankan kembali jika perusahaanData berubah
+
+  const fetchData = async () => {
+    try {
+      const result = await axios.get("http://127.0.0.1:8000/api/perusahaan");
+      setPerusahaanData(result.data.data);
+    } catch (err) {
+      console.log('ada yang salah', err);
+    }
+  }
+  
   return (
     <>
       <div className="card-admin flex flex-col gap-y-5">
@@ -11,7 +34,9 @@ const Perusahaan = () => {
           <HeadPage.SearchAdd></HeadPage.SearchAdd>
         </HeadPage>
       </div>
-      <CardPerusahaan></CardPerusahaan>
+      {perusahaanData.map((perusahaan, i) => (
+        <CardPerusahaan key={i} perusahaanData={perusahaan}></CardPerusahaan>
+      ))}
     </>
   );
 };
--- src/component/Admin/CardPerusahaan.jsx
+++ src/component/Admin/CardPerusahaan.jsx
@@ -1,34 +1,53 @@
-import { IconEdit, IconTrash } from "@tabler/icons-react";
-
-export default function CardPerusahaan()
-{
-    return(
-        <a href="">
-            <div className="bg-white my-5 font-jura border-s-4 border-black shadow-md card-admin">
-                <div className="flex flex-wrap justify-between items-center p-3 py-5">
-                    <div className="flex gap-2 items-center">
-                        <div className="">
-                            <img src="/Img/logo3.jpg" className="w-24 rounded-full" alt="" />
-                        </div>
-                        <div className="">
-                            <h1 className="text-2xl mb-2 font-bold">PT Endfield Industries</h1>
-                            <p>Jl. Raya Cikarang Kemayoran No 27, Kecamatan Beji</p>
-                        </div>
-                    </div>
-                    <div className="text-center">
-                        <div className="">
-                            <a href="" className="bg-black text-white p-2 rounded-full flex mb-2">
-                                <IconEdit/>
-                            </a>
-                        </div>
-                        <div className="">
-                            <a href="" className="border border-black p-2 rounded-full flex hover:bg-black hover:text-white transition ease-in-out delay-150">
-                                <IconTrash/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+import { IconEdit, IconTrash } from '@tabler/icons-react';
+import axios from 'axios';
+// import axios from "axios"
+// import { useEffect, useState } from "react"
+export default function CardPerusahaan({ perusahaanData }) {
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete('http://127.0.0.1:8000/api/perusahaanDelete/' + id);
+      console.log(`Perusahaan dengan ID ${id} telah dihapus.`);
+      // Setelah penghapusan berhasil, Anda dapat memperbarui state atau melakukan hal lain yang diperlukan.
+    } catch (err) {
+      console.error('Gagal menghapus perusahaan', err);
+    }
+  };
+  return (
+    <div>
+      <div className="bg-white my-5 font-jura border-s-4 border-black shadow-md card-admin">
+        <div className="flex flex-wrap justify-between items-center p-3 py-5">
+          <div className="flex gap-2 items-center">
+            <div className="">
+              <img src="/Img/logo3.jpg" className="w-24 rounded-full" alt="" />
             </div>
-        </a>
-    )
+            <div className="">
+              <h1 className="text-2xl mb-2 font-bold">{perusahaanData.nama_perusahaan}</h1>
+              <p>{perusahaanData.alamat_perusahaan}</p>
+            </div>
+          </div>
+          <div className="text-center">
+            <div className="">
+              <a href="" className="bg-black text-white p-2 rounded-full flex mb-2">
+                <IconEdit />
+              </a>
+            </div>
+            <div className="z-50">
+              <button
+                onClick={() => {
+                  const confirmation = window.confirm(`Apakah Anda yakin ingin menhapus perusahaan ini`);
+                  if (confirmation) {
+                    handleDelete(perusahaanData.id_perusahaan);
+                  }
+                }}
+                href=""
+                className="border border-black p-2 rounded-full flex hover:bg-black hover:text-white transition ease-in-out delay-150"
+              >
+                <IconTrash />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
